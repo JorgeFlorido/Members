a3baf2d3ce49452ee8eb69b3161f741ea5acadd3
Jorge Florido <jorgeflorido@outlook.com>
2018-10-21 15:10:16 +0200

    first commit

---
+++ Members/Models/EF/Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Members.Models.EF
+{
+    public class Member
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [StringLength(64, MinimumLength = 5)]
+        public string Address { get; set; }
+    }
+}
---
+++ Members/Models/EF/MembersDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Members.Models.EF
+{
+    public class MembersDbContext : DbContext
+    {
+        public MembersDbContext():base("name = MembersDbContext")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Member> Members { get; set; }
+    }
+}
